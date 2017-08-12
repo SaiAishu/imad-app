3afae9d60843466075dab6f819bd8967e456f097
SaiAishu <30656747+SaiAishu@users.noreply.github.com>
2017-08-12 11:45:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,12 +14,11 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-2',function(req,res){
-    res.send("Article two will be here");
+    res.sendFile(path.join(__dirname,'ui',article-2));
 });
 
-
 app.get('/article-3',function(req,res){
-    res.send("Article three will be here");
+    res.sendFile(path.join(__dirname,'ui','article-3'));
 });
 
 app.get('/ui/style.css', function (req, res) {
