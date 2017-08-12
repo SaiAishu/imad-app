93d7712b6c5847c8bbb664692b4436e699cc8ee3
SaiAishu <30656747+SaiAishu@users.noreply.github.com>
2017-08-12 23:22:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articlename',function(req,res){
-    var articlename;
+    var articlename=req.params.articlename;
     res.send(template(articles[articlename]));
 });
 
