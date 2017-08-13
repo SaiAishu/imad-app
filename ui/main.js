de12df76cbb6538717d7448d663a3ac18ae4f299
SaiAishu <30656747+SaiAishu@users.noreply.github.com>
2017-08-13 20:47:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ console.log('Loaded!');
 
 alert('Page has loaded');
 
-var ele = document.getElementById(txt).innerHTML;
-ele='Showing Progress';
+document.getElementById('txt').innerHTML='Showing Progress';
+
