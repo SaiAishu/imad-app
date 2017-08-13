65b2b65849126fc5650dc537cc8d295ebf4e0646
SaiAishu <30656747+SaiAishu@users.noreply.github.com>
2017-08-13 21:13:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,15 @@ alert('Page has loaded');
 document.getElementById('txt').innerHTML='Showing Progress';
 
 var img=document.getElementById('madi');
+
+var mar=0;
+function move(){
+    mar+=10;
+    img.style.marginLeft=mar+'px';
+}
+
 img.onclick = function (){
-    img.style.marginLeft = '200px';
+  //  img.style.marginLeft = '200px';
+    setInterval(move,100);
+    
 };
