2ae5790d4274ffc12cc0e68e1f06c9fe720435a8
SaiAishu <30656747+SaiAishu@users.noreply.github.com>
2017-08-13 21:15:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,12 +8,12 @@ var img=document.getElementById('madi');
 
 var mar=0;
 function move(){
-    mar+=50;
+    mar+=10;
     img.style.marginLeft=mar+'px';
 }
 
 img.onclick = function (){
   //  img.style.marginLeft = '200px';
-    setInterval(move,100);
+    setInterval(move,10);
     
 };
