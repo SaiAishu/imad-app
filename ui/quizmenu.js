b4a82ebb25199c222d50a8b4b9dbf16d6a67118a
SaiAishu <30656747+SaiAishu@users.noreply.github.com>
2017-10-10 20:28:39 +0530

    [imad-console] Updates ui/quizmenu.js

--- ui/quizmenu.js
+++ ui/quizmenu.js
@@ -1,3 +1,10 @@
+
+function ani(){
+        $('.menu').animate({left:'0px'},200);
+        $('body').animate({left:'256px'},200);
+    }
+
+
 var main=function(){
     $('.icon-menu').click(function(){
         $('.menu').animate({left:'0px'},200);
