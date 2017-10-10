46c27c6fd8458eca1a7a36a3e6475cd304419829
SaiAishu <30656747+SaiAishu@users.noreply.github.com>
2017-10-10 21:48:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,11 +60,11 @@ request.send('');
 
 function ani(){
         $('.menu').animate({left:'0px'},200);
-        $('body').animate({left:'256px'},200);
+        $('body').animate({left:'265px'},200);
     }
 
 function closemenu(){
-    $('.menu').animate({left:'-256px'},200);
+    $('.menu').animate({left:'-265px'},200);
     $('body').animate({left:'0px'},200);
     
 }
