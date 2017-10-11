18dcb199825cc837d11d2c2d19e2d6ca25b18f5b
SaiAishu <30656747+SaiAishu@users.noreply.github.com>
2017-10-11 09:38:18 +0530

    [imad-console] Updates ui/quiz.js

--- ui/quiz.js
+++ ui/quiz.js
@@ -1,1 +1,13 @@
 
+
+function ani(){
+        $('.menu').animate({left:'0px'},200);
+        $('body').animate({left:'265px'},200);
+    }
+
+function closemenu(){
+    $('.menu').animate({left:'-265px'},200);
+    $('body').animate({left:'0px'},200);
+    
+}
+$(document).ready(main)
